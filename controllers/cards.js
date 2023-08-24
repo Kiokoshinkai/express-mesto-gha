9029a6ca8522a181fbb8f47389d87ad7eba9b5ec
Kiokoshinkai <polnochnamarse@gmail.com>
2023-08-24 18:24:13 +0300

    feat:server done

--- controllers/cards.js
+++ controllers/cards.js
@@ -11,39 +11,67 @@ const getCards = (req, res, next) => {
 const createCard = (req, res, next) => {
   const { name, link } = req.body;
   const owner = req.user._id;
+  const ERROR_CODE = 400;
 
   Card.create({ name, link, owner })
     .then((card) => res.status(201).send(card))
-    .catch((err) => next(err));
+    .catch((err) => {
+      if (err.name === 'SomeErrorName') {
+        res.status(ERROR_CODE).send('Переданы неверные данные');
+      } else {
+        next(err);
+      }
+    });
 };
 
 // удаляет карточку по идентификатору
 const deleteCard = (req, res, next) => {
   const { cardId } = req.params;
+  const ERROR_CODE = 404;
 
   Card.deleteOne({ _id: cardId })
     .then(() => res.status(200))
-    .catch((err) => next(err));
+    .catch((err) => {
+      if (err.name === 'SomeErrorName') {
+        res.status(ERROR_CODE).send('Запрашиваемый пользователь не найден');
+      } else {
+        next(err);
+      }
+    });
 };
 
 // поставить лайк карточке
 const addLike = (req, res, next) => {
   const { cardId } = req.params;
   const userId = req.user._id;
+  const ERROR_CODE = 404;
 
   Card.findByIdAndUpdate(cardId, { $addToSet: { likes: userId } }, { new: true })
     .then((card) => res.status(200).send(card))
-    .catch((err) => next(err));
+    .catch((err) => {
+      if (err.name === 'SomeErrorName') {
+        res.status(ERROR_CODE).send('Запрашиваемый пользователь не найден');
+      } else {
+        next(err);
+      }
+    });
 };
 
 // убрать лайк с карточки
 const removeLike = (req, res, next) => {
   const { cardId } = req.params;
   const userId = req.user._id;
+  const ERROR_CODE = 404;
 
   Card.findByIdAndUpdate(cardId, { $pull: { likes: userId } }, { new: true })
     .then((card) => res.status(200).send(card))
-    .catch((err) => next(err));
+    .catch((err) => {
+      if (err.name === 'SomeErrorName') {
+        res.status(ERROR_CODE).send('Запрашиваемый пользователь не найден');
+      } else {
+        next(err);
+      }
+    });
 };
 
 module.exports = {
--- controllers/users.js
+++ controllers/users.js
@@ -10,15 +10,23 @@ const getUsers = (req, res, next) => {
 // возвращает пользователя по _id
 const getUserById = (req, res, next) => {
   const { userId } = req.params;
+  const ERROR_CODE = 404;
 
   User.findById(userId)
     .then((user) => res.status(200).send(user))
-    .catch((err) => next(err));
+    .catch((err) => {
+      if (err.name === 'SomeErrorName') {
+        res.status(ERROR_CODE).send('Запрашиваемый пользователь не найден');
+      } else {
+        next(err);
+      }
+    });
 };
 
 // создаёт пользователя
 const createUser = (req, res, next) => {
   const { name, about, avatar } = req.body;
+  const ERROR_CODE = 400;
 
   User.create({ name, about, avatar })
     .then((user) => res.status(200).send({
@@ -27,27 +35,47 @@ const createUser = (req, res, next) => {
       about: user.about,
       avatar: user.avatar,
     }))
-    .catch((err) => next(err));
+    .catch((err) => {
+      if (err.name === 'SomeErrorName') {
+        res.status(ERROR_CODE).send('Переданы неверные данные пользователя');
+      } else {
+        next(err);
+      }
+    });
 };
 
 // обновляет профиль
 const updateProfile = (req, res, next) => {
   const { name, about } = req.body;
   const userId = req.user._id;
+  const ERROR_CODE = 400;
 
   User.findByIdAndUpdate(userId, { name, about })
     .then((user) => res.status(200).send(user))
-    .catch((err) => next(err));
+    .catch((err) => {
+      if (err.name === 'SomeErrorName') {
+        res.status(ERROR_CODE).send('Переданы неверные данные пользователя');
+      } else {
+        next(err);
+      }
+    });
 };
 
 // обновляет аватар
 const updateAvatar = (req, res, next) => {
   const { avatar } = req.body;
   const userId = req.user._id;
+  const ERROR_CODE = 400;
 
   User.findByIdAndUpdate(userId, { avatar })
     .then((user) => res.status(200).send(user))
-    .catch((err) => next(err));
+    .catch((err) => {
+      if (err.name === 'SomeErrorName') {
+        res.status(ERROR_CODE).send('Переданы неверные данные пользователя');
+      } else {
+        next(err);
+      }
+    });
 };
 
 module.exports = {
